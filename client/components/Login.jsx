d973f3f173559b790a476d7fae08add7ed198b19
Omar <omar_rana93@hotmail.com>
2021-08-28 15:33:45 -0700

    Day 2 Progress
    Co-authored by Matthew Lee
    Co-authored by Brian Cheng
    Co-authored by Josh Cretella
    Co-authored by Philip Hua

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,23 +1,46 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import fetch from 'node-fetch';
+
 
 const Login = () => {
   let history = useHistory();
 
-  const onSubmit = () => {
-    history.push('/dash/')
+  // const onSubmit = () => {
+  //   fetch('/oauth')
+
+  //   // // history.push('/dash/')
+  // }
+  const getUserInfo = (access_code) => {
+    console.log('invoking getUserInfo');
+    fetch('/google', {
+      method: 'GET',
+      headers: { access_code: access_code }
+    })
+      .then(res => res.json())
+      .then(res => console.log(res))
+      .catch(err => console.log('ERRRR', err))
+  }
+
+
+  if (window.location.hash) {
+    const hash = (new URL(document.location)).hash;
+    const access_code = hash.match(/(?<=#access_token=)(.*)(?=&token_type)/)[0];
+    console.log('access_code: ', access_code);
+    getUserInfo(access_code);
   }
 
   return (
-  <div>
-    <form  onSubmit={() => onSubmit()} >
-      <label htmlFor="login_username">Username</label>
-      <input id="login_username" type="text" defaultValue="enter username here" />
-      <input id="login_password" type="text" defaultValue="enter password here" />
-      
-      <button id="login_button" type="submit" value="login">login</button>
-    </form>
-  </div>
+    <div>
+      {/* <form onSubmit={() => onSubmit()} > */}
+      <form id="googleLogin" action='/oauth' method='GET'>
+        <label htmlFor="login_username">Username</label>
+        <input id="login_username" type="text" defaultValue="enter username here" />
+        <input id="login_password" type="text" defaultValue="enter password here" />
+
+        <button id="login_button" type="submit" value="login">login</button>
+      </form>
+    </div>
   )
 }
 
